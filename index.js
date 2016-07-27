29ce11abc3849e4ecf4583629fab8e5c70216b70
aleeex <akang2@gmail.com>
2016-07-27 10:27:52 -0600

    ran npm init and then npm install --save express

---
+++ index.js
@@ -0,0 +1,7 @@
+/*go to the root of your new project in terminal,
+then type 'npm init'
+You'll then see you got a package.json file.
+Then type 'npm install --save express'
+You'll then see that there's a node_modules folder
+
+*/
